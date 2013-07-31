b0dd1895acfe07a1188113ac16439a8485ba1795
Beryl <beryl.schragger@gmail.com>
2013-07-30 22:10:17 -0400

    Add hover function, continue card click function - cannot find a way to remove text slowly (or after a delay) without removing card.

--- assignments/memory-student/memory.js
+++ assignments/memory-student/memory.js
@@ -22,15 +22,22 @@ var lastId = '',
 
 //Code In Here gets executed once code is ready. ie hovering, clicking events//
 $(function() {
+  // using medium size array for now
+  // will need to select array with click functions later
   letters = _.shuffle(lettersMedium);
   startGame();
   cardClick();
+  hovering();
 });
 
 // Initializes the game and creates the board
 function startGame() {
+  // prints letters array to console - remove later
   console.log(letters);
-  $.each(letters, function(index){
+  // creates div for each letter, adds square class to each
+  // adds unique id to each div to match a letter's index
+  // adds div into game div
+  $.each(letters, function(index, value){
     var div = $('<div/>').addClass('square');
     div.attr("id", index);
     div.appendTo('#game');
@@ -39,14 +46,45 @@ function startGame() {
 
 // Flips a card and checks for a match
 function cardClick() {
+  // occurs when div with square class is clicked
   $('.square').click(function() {
-    console.log(div);
+    // rest of function only runs if this square is not the one previously clicked
+    if (this.id !== lastId){
+      // takes div id, finds corresponding letter in letters array, adds letter as text of div
+      var index = parseInt(this.id);
+      $(this).text(letters[index]);
+      // if lastId is blank, add clicked div's info to lastId and lastCard
+      if (lastId === '') {
+        lastCard = $(this).text();
+        lastId = this.id;
+      }
+      // if div's text matches that of the last square clicked, found class is added to both squares
+      else {
+        if ($(this).text() === lastCard) {
+          $(this).addClass('found');
+          $('#' + lastId).addClass('found');
+        }
+        // if lastId is not blank, both squares should be made blank after a delay
+        else {
+          setTimeout(function(){$('#' + lastId).text("")}, 1000);
+          setTimeout(function(){$(this).text("")}, 1000);
+        }
+        // lastCard and lastId should be made blank after any two selections, right or wrong
+        lastCard = '';
+        lastId = '';
+      }
+    }
   });
 }
 
 //Add hoverclass to cards.
 function hovering() {
-
+  // occurs when div with square class is moused on/off
+  // adds and removes the hover class, which changes div colors
+  $('.square').hover(
+    function() {$(this).addClass('hover');},
+    function() {$(this).removeClass('hover');}
+  );
 }
 
 //Start the timer
