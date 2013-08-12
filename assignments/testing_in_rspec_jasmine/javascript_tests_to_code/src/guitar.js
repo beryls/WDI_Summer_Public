7a8c1467f11fb00f68c1c523a2122fceabd17558
Beryl <beryl.schragger@gmail.com>
2013-08-12 13:48:23 -0400

    Adds to testing in rspec/jasmine file.

--- assignments/testing_in_rspec_jasmine/javascript_tests_to_code/src/guitar.js
+++ assignments/testing_in_rspec_jasmine/javascript_tests_to_code/src/guitar.js
@@ -7,4 +7,33 @@ if (typeof Object.create !== 'function') {
 }
 
 var guitar = {
+  name: "Fender Esquire",
+  setName: function(name){
+    this.name = name;
+  },
+  numberOfStrings: 6,
+  volume: 5,
+  breakString: function(){
+    if (this.numberOfStrings > 0) {
+      this.numberOfStrings--;
+    }
+    if (this.numberOfStrings === 0) {
+      this.volume = 0;
+    }
+  },
+  restring: function() {
+    this.numberOfStrings = 6;
+    this.volume = 5;
+  },
+  playRock: function() {
+    if (this.numberOfStrings > 0) {
+      this.volume = 11;
+    }
+  },
+  playBasicRhythm: function() {
+    return "jug jigga jug jigga jug"
+  },
+  playHighStrings: function() {
+    return "meedley meedley meedley meedley meedley meedley meedley meedley meedley meedley meedley meedley MEEEEEEEEEEEEEEEE. \'And the dragon comes in the NIIIiiiiIIIiiiiIIIIIIIIiiiiiiiiiiiIIIIIIIIiiiIIGGHH\'";
+  }
 };
